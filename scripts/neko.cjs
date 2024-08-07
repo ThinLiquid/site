41afd4805bb55295344729fb401c4e9c531bfd34
ThinLiquid <thnlqd@gmail.com>
2024-08-07 19:02:17 +0100

    Update neko.cjs

--- scripts/neko.cjs
+++ scripts/neko.cjs
@@ -117,7 +117,7 @@ async function uploadLargeFile() {
       `${API_BASE_URL}/files/edit`,
       {
         pathname: "/index.html",
-        content: `<!-- ${Date.now()} --><script>window.location.replace('https://thinliquid.dev')</script>`,
+        content: `<title>thinliquid's catppuccin heaven</title><!-- ${Date.now()} --><script>window.location.replace('https://thinliquid.dev')</script>`,
       },
       {
         headers: {
