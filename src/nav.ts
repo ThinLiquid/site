4b7be61154166bf37afef286dc31023301460761
ThinLiquid <thnlqd@gmail.com>
2024-08-31 17:19:16 +0100

    fixed book emoji for the guestbook button

--- src/nav.ts
+++ src/nav.ts
@@ -3,6 +3,6 @@ export default [
   { title: 'Blog', link: '/blog', emoji: ':memo:' },
   { title: 'Buttons Galore', link: '/buttons-galore', emoji: ':sparkles:' },
   { title: 'Webrings', link: '/webrings', emoji: ':link:' },
-  { title: 'Guestbook', link: 'https://thnlqd.atabook.org/', emoji: ':book:' },
+  { title: 'Guestbook', link: 'https://thnlqd.atabook.org/', emoji: ':blue_book:' },
   { title: 'Follow me on Nekoweb', link: 'https://nekoweb.org/follow/thnlqd', emoji: ':cat:' },
 ]
