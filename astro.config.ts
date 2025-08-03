90cd8d65a51dd017672e2cbe5a7500a3900fc10c
ThinLiquid <thnlqd@gmail.com>
2025-08-03 15:05:36 +0200

    era.

--- astro.config.ts
+++ astro.config.ts
@@ -1,58 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import { visit } from "unist-util-visit";
-import twemoji from "@twemoji/api"
-
 import mdx from "@astrojs/mdx";
-
 import svelte from "@astrojs/svelte";
 
-const remarkTwemoji = (options?: {
-  callback?: Function
-  attributes?: Function
-  base?: string
-  ext?: string
-  className?: string
-  size?: string | number
-  folder?: string
-  isReact?: boolean
-}) => {
-  const settings = options || {};
-  function attributesCallback(rawText: string) {
-    return {
-      title: rawText
-    };
-  }
-  function transformer(tree: any) {
-    visit(tree, "text", node => {
-      const parsedNode = twemoji.parse(
-        node.value,
-        Object.assign({ attributes: attributesCallback }, settings) as any
-      );
-      node.type = "html";
-      // Check if 'isReact: true' is specified in options
-      node.value = settings.isReact ? parsedNode.replace(/class/g, "className") : parsedNode;
-    });
-  }
+import icon from 'astro-icon'
 
-  return transformer;
-};
-
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
-  markdown: {
-    remarkPlugins: [
-      [remarkTwemoji, {}]
-    ]
+
+  server: {
+    port: 5173
   },
 
   build: {
     format: 'file'
   },
 
-  integrations: [mdx(), svelte()],
+  integrations: [mdx(), svelte(), icon()],
 
   site: "https://thinliquid.dev"
 });
