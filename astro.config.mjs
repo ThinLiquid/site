68effecd138f87315acc702d2e30da33a7efb293
ThinLiquid <thnlqd@gmail.com>
2025-08-09 15:15:38 +0200

    optimizations

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+import compressor from 'astro-compressor';
+// https://astro.build/config
+export default defineConfig({
+  integrations: [compressor()]
+});
